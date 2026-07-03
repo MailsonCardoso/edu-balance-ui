c219c843866619d78845835e4f1cf80fcd52159c
Mailson <mailsonccardoso@gmail.com>
2026-07-02 21:25:05 -0300

    Revert "rebuild dist"
    This reverts commit 274dc357d1eb0cda968e629296e713e5aa1faab1.

---
+++ dist/assets/financeiro-DuOUszaR.js
@@ -0,0 +1,1 @@
+import{j as t,O as n}from"./index-DyqTKKZT.js";function r(){return t.jsx("div",{children:t.jsx(n,{})})}export{r as component};
--- dist/assets/financial-categories-CKMLVYZR.js
+++
@@ -1,1 +0,0 @@
-import{K as a}from"./index-Dsdm8xA2.js";async function e(){try{const{data:t}=await a.get("/financial-categories");return t}catch{return[]}}export{e as f};
