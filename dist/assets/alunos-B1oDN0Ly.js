274dc357d1eb0cda968e629296e713e5aa1faab1
Mailson <mailsonccardoso@gmail.com>
2026-07-02 21:17:48 -0300

    rebuild dist

--- dist/assets/alunos-B1oDN0Ly.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-DyqTKKZT.js";const n=()=>t.jsx(o,{});export{n as component};
---
+++ dist/assets/financial-categories-CKMLVYZR.js
@@ -0,0 +1,1 @@
+import{K as a}from"./index-Dsdm8xA2.js";async function e(){try{const{data:t}=await a.get("/financial-categories");return t}catch{return[]}}export{e as f};
