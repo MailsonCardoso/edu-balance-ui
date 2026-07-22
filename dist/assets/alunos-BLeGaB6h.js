4667acad95c73f69d35525fe150615c86517eb9e
Mailson <mailsonccardoso@gmail.com>
2026-07-22 08:58:06 -0300

    fix: restore sidebar scrollbar and fix background color
    - Change ScrollArea type from 'never' to 'hover' to show scrollbar on hover
    - Add bg-sidebar to ScrollArea to fix background color mismatch
    - Bump version to v2.2.2

--- dist/assets/alunos-BLeGaB6h.js
+++
@@ -1,1 +0,0 @@
-import{j as t,aa as o}from"./index-BwgYl0fa.js";const n=()=>t.jsx(o,{});export{n as component};
---
+++ dist/assets/financeiro-BtvxphEn.js
@@ -0,0 +1,1 @@
+import{j as t,aa as n}from"./index-CnC9RUKN.js";function r(){return t.jsx("div",{children:t.jsx(n,{})})}export{r as component};
