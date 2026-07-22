3d95395f982651deb742b0d3a63968f182b20546
Mailson <mailsonccardoso@gmail.com>
2026-07-22 08:06:17 -0300

    chore: bump version to v2.2.0

--- dist/assets/alunos-Cy-cT0gq.js
+++
@@ -1,1 +0,0 @@
-import{j as t,a9 as o}from"./index-fHhYG9CJ.js";const n=()=>t.jsx(o,{});export{n as component};
--- dist/assets/financial-categories-D3LFsENO.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./index-fHhYG9CJ.js";async function e(){try{const{data:t}=await a.get("/financial-categories");return t}catch{return[]}}export{e as f};
