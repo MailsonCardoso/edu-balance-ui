d18d0ad0eb84e1de253f0c396ad3ad2cfe49a4c8
Mailson <mailsonccardoso@gmail.com>
2026-07-25 01:02:08 -0300

    remove scheduler backend e limpa banco para inicio em agosto

--- back_bombeiro/routes/console.php
+++ back_bombeiro/routes/console.php
@@ -1,8 +1,1 @@
 <?php
-
-use Illuminate\Support\Facades\Schedule;
-
-Schedule::command('mensalidades:gerar-proximo-mes')
-    ->monthlyOn(26, '00:00')
-    ->withoutOverlapping()
-    ->appendOutputTo(storage_path('logs/mensalidades.log'));
