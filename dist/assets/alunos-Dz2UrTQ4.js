c1adc061f1100cd58f390a370811f09101c7ec25
Mailson <mailsonccardoso@gmail.com>
2026-07-26 02:25:34 -0300

    fallback categoriasPadrao para funcionar sem seed no backend

--- dist/assets/alunos-Dz2UrTQ4.js
+++
@@ -1,1 +0,0 @@
-import{j as t,aa as o}from"./index-CTlW5pfs.js";const n=()=>t.jsx(o,{});export{n as component};
--- dist/assets/loader-circle-zzonHJ-t.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-CTlW5pfs.js";const o=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],a=e("loader-circle",o);export{a as L};
